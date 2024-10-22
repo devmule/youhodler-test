ad13e99d5fa05ec8ac323554b6eb326275487aea
Maxim Batyatin <35449804+devmule@users.noreply.github.com>
2024-10-22 19:56:45 +0400

    added docker

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const BINANCE_API_URL =
 export const BINANCE_API_KEY = process.env.BINANCE_API_KEY;
 export const BINANCE_API_SECRET = process.env.BINANCE_API_SECRET;
 export const BINANCE_COMMISSION =
-  Number(process.env.BINANCE_COMMISSION ?? "0.01") / 100;
+  Number(process.env.BINANCE_COMMISSION_PCT ?? "0.01") / 100;
 export const APP_PORT = process.env.APP_PORT ?? "3000";
 export const APP_CACHE_DURATION_MS = Number(
   process.env.APP_CACHE_DURATION_MS ?? "10000",
